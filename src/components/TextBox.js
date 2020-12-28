0a868c8d5f00b8dc259b8de65beb71df86578dd1
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2020-12-28 21:29:39 +0700

    textarea and send button

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import {
 	Box,
+	Button,
+	Flex,
 } from 'rebass'
-import { Label, Input } from '@rebass/forms'
+import { Label, Input, Textarea } from '@rebass/forms'
 import { useTheme } from '@emotion/react'
 
 const TextBox = () => {
 	const theme = useTheme()
 	return (
-		<>
+		<Flex
+			theme={theme}
+			flexGrow='1'
+			flexDirection='column'
+			sx={{
+				'*': {
+					mb: '.5em'
+				}
+			}}
+			>
 			<Box sx={{py: '2em'}} theme={theme}>
-				<Label theme={theme} htmlFor='email'>Type Here</Label>
+				<Label theme={theme} htmlFor='title'>Title</Label>
 				<Input
-					id='text'
-					name='text'
-					type='text'
+					id='title'
+					name='title'
+					type='title'
+					mb = '1em'
+					theme={theme}
+				/>
+
+				<Textarea
+					id='message'
+					name='message'
 					placeholder='some of your thoughts...'
 					theme={theme}
 				/>
+				<Button theme={theme} float='right'>Send</Button>
 			</Box>
-		</>
+		</Flex>
 	)
 }
 
