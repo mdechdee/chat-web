b3b02a1f01cc1fbbc390fd7ffe8a9b2d352e01b8
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2021-01-12 20:13:04 +0700

    Update VisitorPage.js

--- src/components/VisitorPage.js
+++ src/components/VisitorPage.js
@@ -4,34 +4,29 @@ import { useTheme } from '@emotion/react';
 import { useHistory } from 'react-router-dom';
 
 const VisitorPage = props => {
-    let history = useHistory();
-    const signup = () => {
-        console.log("signup clicked");
-        history.push({
-            pathname: '/signup'
-        });
-    }
-    const signin = () => {
-        history.push({
-            pathname: '/login'
-        });
-    }
-    const theme = useTheme();
-    return (
-        <>  
-            <Flex
-                sx={{
-                flexDirection: 'column'
-            }}>
-                <Box p={120}>
-                    <Text fontSize='5em' fontWeight='bold' textAlign='center' > ChatWithMe </Text>
-                    <Text fontSize='2em' textAlign='center'> A simple chat website for everyone </Text>
-                </Box>
-                <Button theme={theme} onClick={signup} sx={{ mt: '1em' }}>Sign up</Button>
-                <Button theme={theme} onClick={signin} sx={{ mt: '1em' }}>Login</Button>
-            </Flex>
-        </>
-    );
+	let history = useHistory();
+	const signup = () => {
+		console.log("signup clicked");
+		history.push({
+			pathname: '/signup'
+		});
+	}
+	const signin = () => {
+		history.push({
+			pathname: '/login'
+		});
+	}
+	const theme = useTheme();
+	return (
+		<>
+			<Box p={120}>
+				<Text fontSize='5em' fontWeight='bold' textAlign='center' > ChatWithMe </Text>
+				<Text fontSize='2em' textAlign='center'> A simple chat website for everyone </Text>
+			</Box>
+			<Button theme={theme} onClick={signup} sx={{ mt: '1em' }}>Sign up</Button>
+			<Button theme={theme} onClick={signin} sx={{ mt: '1em' }}>Login</Button>
+		</>
+	);
 }
 
 export default VisitorPage;
